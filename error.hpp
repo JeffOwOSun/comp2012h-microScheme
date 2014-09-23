69d048fe2e4bc4f1440e36c29b2034114657c65a
SUN Yushi <sunyushi9546@gmail.com>
2014-09-23 17:21:27 +0800

    new error handler with customized message

--- error.hpp
+++ error.hpp
@@ -7,3 +7,8 @@ inline void error_handler() {
   std::cerr << "ERROR" << std::endl;
   exit(1);
 }
+
+inline void error_handler(std::string err_msg){
+  std::cerr << err_msg << std::endl;
+  exit(1);
+}
