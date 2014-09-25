8133b4414087da9400ef41474588bbe006e7c2c1
SUN Yushi <sunyushi9546@gmail.com>
2014-09-25 17:22:24 +0800

    use default value instead of overloading

--- error.hpp
+++ error.hpp
@@ -3,12 +3,7 @@
  * \brief handles error very basically
  */
 
-inline void error_handler() {
-  std::cerr << "ERROR" << std::endl;
-  exit(1);
-}
-
-inline void error_handler(std::string err_msg){
+inline void error_handler(std::string err_msg="ERROR"){
   std::cerr << err_msg << std::endl;
   exit(1);
 }
