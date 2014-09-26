b5350b6e8c991a3d872ebabde3e24ce74cbf1d5f
SUN Yushi <sunyushi9546@gmail.com>
2014-09-26 15:36:32 +0800

    improve error

--- error.hpp
+++ error.hpp
@@ -3,7 +3,11 @@
  * \brief handles error very basically
  */
 
-inline void error_handler(std::string err_msg="ERROR"){
-  std::cerr << err_msg << std::endl;
+inline void error_handler(std::string err_msg=""){
+  if (err_msg == "") {
+    std::cerr << "ERROR" << std::endl;
+  } else {
+    std::cerr << "ERROR: " << err_msg <<std::endl;
+  }
   exit(1);
 }
