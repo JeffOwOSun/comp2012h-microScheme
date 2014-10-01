926080de03a7ad9da4745e0ff1e2fe017f42efcb
SUN Yushi <sunyushi9546@gmail.com>
2014-10-01 22:27:40 +0800

    migrate code from a1 to a2

---
+++ Cell.cpp
@@ -0,0 +1,190 @@
+/**
+ * \file Cell.cpp
+ *
+ * A partial dummy implementation of the Cell.hpp interface for
+ * example purposes, to support the cons list ADT interface specified
+ * in cons.hpp.  Just barely enough is implemented to allow the parser
+ * to run.  None of the accessor member functions are implemented.
+ *
+ * You need to replace this with a real implementation that actually
+ * builds a tree data structure and then lets the user access the tree.
+ *
+ * You don't necessarily need to understand the ugly details of this
+ * dummy implementation, which you'll throw away anyhow.
+ */
+
+#include "Cell.hpp"
+// Reminder: cons.hpp expects nil to be defined somewhere.  For this
+// implementation, this is the logical place to define it.
+Cell* const nil = new Cell(0);
+
+using namespace std;
+
+Cell::Cell(const int i):tag_m(type_int),int_m(i)
+{}
+
+Cell::Cell(const double d):tag_m(type_double), double_m(d)
+{}
+
+Cell::Cell(const char* const s)
+{
+  tag_m = type_symbol;
+  symbol_m = new char[strlen(s)+1];
+  strcpy (symbol_m, s);
+  return;
+}
+
+Cell::Cell(Cell* const my_car, Cell* const my_cdr):tag_m(type_conspair)
+{
+  conspair_m.car_m = my_car;
+  conspair_m.cdr_m = my_cdr;
+}
+
+bool Cell::is_int() const
+{
+  return tag_m == type_int;
+}
+
+bool Cell::is_double() const
+{
+  return tag_m == type_double;
+}
+
+bool Cell::is_symbol() const
+{
+  return tag_m == type_symbol;
+}
+
+bool Cell::is_cons() const
+{
+  return tag_m == type_conspair;
+}
+
+int Cell::get_int() const
+{
+  if (is_int()) {
+    return int_m;
+  } else {
+    cerr << "In function Cell::get_int(), the corresponding Cell instance is not a int Cell!" << endl;
+    exit(1);
+  }
+}
+
+double Cell::get_double() const
+{
+  if (is_double()) {
+    return double_m;
+  } else {
+    cerr << "In function Cell::get_double(), the corresponding Cell instance is not a double Cell!" << endl;
+    exit(1);
+  }
+}
+
+string Cell::get_symbol() const
+{
+  if (is_symbol()) {
+    return string(symbol_m);
+  } else {
+    cerr << "In function Cell::get_symbol(), the corresponding Cell instance is not a symbol Cell!" << endl;
+    exit(1);
+  }
+}
+
+Cell* Cell::get_car() const
+{
+  if (is_cons()) {
+    return conspair_m.car_m;
+  } else {
+    cerr << "In function Cell::get_car(), the corresponding Cell is not a cons Cell!" << endl;
+    exit(1);
+  }
+}
+
+Cell* Cell::get_cdr() const
+{
+  if (is_cons()) {
+    return conspair_m.cdr_m;
+  } else {
+    cerr << "In function Cell::get_cdr(), the corresponding Cell is not a cons Cell!" << endl;
+    exit(1);
+  }
+}
+
+void Cell::print(ostream& os) const
+{
+  if (this == nil) {
+    error_handler("Trying to print nil!");
+  }
+  
+  if (is_int() || is_double() || is_symbol()) {
+    os << to_string();
+  } else if (is_cons()) {
+    os << "(";
+    const Cell* current_cell = this;
+
+    while (current_cell!=nil){
+      //get car
+      //if car is conspair, call print on the subtree
+      if (current_cell->get_car()->is_cons()){
+	current_cell->get_car()->print(os);
+      } else{
+	//else get the value and output it
+	os << current_cell->get_car()->to_string();
+      }
+      //loop to the cdr
+      current_cell = current_cell->get_cdr();
+
+      if (current_cell != nil) {
+	os << " ";
+      }
+    }
+    os << ")";
+  }
+}
+
+string Cell::to_string() const
+{
+  stringstream ss;
+  if (is_int()) {
+    ss << get_int();
+  } else if (is_double()) {
+    ss << fixed << setprecision(6) << get_double();
+  } else if (is_symbol()) {
+    ss << get_symbol();
+  } else ss << "";
+  return ss.str();
+}
+
+Cell::Cell(const Cell& c)
+{
+  if (&c == nil) error_handler("trying to deep copy a nil Cell!");
+  
+  if (c.is_int()){
+    tag_m = type_int;
+    int_m = c.get_int();
+  } else if (c.is_double()){
+    tag_m = type_double;
+    double_m = c.get_double();
+  } else if (c.is_symbol()){
+    tag_m = type_symbol;
+    symbol_m = new char[c.get_symbol().size()+1];
+    strcpy(symbol_m, c.get_symbol().c_str());
+  } else if (c.is_cons()){
+    tag_m = type_conspair;
+    conspair_m.car_m = c.get_car();
+    conspair_m.cdr_m = c.get_cdr();
+  }
+}
+
+Cell::~Cell(){
+  
+  //prevent deletion of nil
+  if (this == nil) return;
+
+  if (is_cons()){
+    delete get_car();
+    delete get_cdr();
+  } else if (is_symbol()) {
+    delete[] symbol_m;
+  }
+}
---
+++ eval.cpp
@@ -0,0 +1,168 @@
+/**
+ * \file eval.cpp
+ *
+ * A dummy implementation for example purposes.  Just barely enough is
+ * implemented to allow the parser to run.  Merely returns the input
+ * expression without evaluating it.
+ */
+
+#include "eval.hpp"
+
+/**
+ * \brief The evaluation function that calculates the parsed s-expression tree
+ * \param c A constant pointer to a Cell instance, which is the root of the tree to be computed
+ * \return A pointer to the Cell containing the final answer.
+ */
+Cell* eval(Cell* const c)
+{
+  if (nullp(c)) {
+    error_handler("s-expression invalid: root of tree is nil");
+  }
+
+  if (intp(c) || doublep(c) || symbolp(c)){
+    return deep_copy(c);
+  }
+  
+  if (listp(c)) {
+    //get car of c
+    //using eval will automatically evaluate the subtree, if car(c) is itself a conspair
+    Cell* car_value = eval(car(c));
+    
+    //if car is a symbol cell (it must, otherwise the eval() begins at wrong place)
+    if (symbolp(car_value)){
+      //get the symbol
+      //case 1: +
+      if (get_symbol(car_value) == "+"){
+	
+	//delete car_value as it's not needed any more
+	delete car_value;
+
+	//temporary sums variables
+	double double_sum = 0;
+	int int_sum = 0;
+	bool sum_is_double = false;
+
+	//Cell pointer to the current working cell
+	Cell* current_cell = cdr(c);
+
+	//iterate every cons pair until meet a nil cdr
+	while (!nullp(current_cell)) {
+
+	  //current_cell is not nil, and it should be a conspair
+	  if (!listp(current_cell)) {
+	    error_handler("cdr must be nil or conspair");
+	  }
+
+	  //pointer to the cell that contains the value to be added
+	  //here eval could be used against a conspair or a int/double cell
+	  Cell* value_cell = eval(car(current_cell));
+	  
+	  //deal with value_cell, see if it's int or not
+	  if (intp(value_cell)) {
+	    if (sum_is_double) {
+	      double_sum += get_int(value_cell);
+	    } else {
+	      int_sum += get_int(value_cell);
+	    }
+	  } else if (doublep(value_cell)) {
+	    //if value_cell is not a double cell
+	    if (sum_is_double) {
+	      double_sum += get_double(value_cell);
+	    } else {
+	      //migrate int_sum to double_sum and do related clean-ups
+	      double_sum = int_sum;
+	      int_sum = 0;
+	      sum_is_double = true;
+	      
+	      double_sum += get_double(value_cell);
+	    }
+	  } else {
+	    if (!nullp(value_cell)) delete value_cell;
+	    error_handler("s-expression invalid: + operands invalid");
+	  }
+	  if (!nullp(value_cell)) delete value_cell;
+	  //move current_cell forward;
+	  current_cell = cdr(current_cell);
+	}
+	
+	return sum_is_double ? make_double(double_sum) : make_int(int_sum);
+      }
+      //case 2: ceiling
+      else if (get_symbol(car_value) == "ceiling") {
+	//delete car_value as it's no longer needed
+	delete car_value;
+
+	//current working cell
+	Cell* current_cell = cdr(c);
+
+	if (nullp(current_cell) || !listp(current_cell)) error_handler("s-expression invalid: invalid ceiling operand!");
+
+	if (!nullp(cdr(current_cell))) error_handler("s-expression invalid: ceiling on more than one operands");
+	
+	//take the ceiling and return
+	Cell* returned_value = eval(car(current_cell));
+	if (intp(returned_value)){
+	  delete returned_value;
+	  error_handler("s-expression invalid: ceiling on integer!");
+	} else if (doublep(returned_value)){
+	  int ceilinged_value = int(get_double(returned_value));
+	  if (ceilinged_value < get_double(returned_value)) ++ceilinged_value;
+	  delete returned_value;
+	  return make_int(ceilinged_value);
+	} else {
+	  if(!nullp(returned_value)) delete returned_value;
+	  error_handler("s-expression invalid: ceiling on symbol!");
+	}
+      }
+      //case 3: if
+      else if (get_symbol(car_value) == "if") {
+	//delete car_value as it's no longer needed
+	delete car_value;
+	
+	//temporary Cell pointers;
+	Cell* condition = cdr(c);
+	if (nullp(condition) || !listp(condition)) error_handler("s-expression invalid: condition is not a conspair");
+	Cell* if_true = cdr(condition);
+	if (nullp(if_true) || !listp(if_true)) error_handler("s-expression invalid: the true return value is not a cospair");
+	Cell* if_false = cdr(if_true); 
+
+	//directly return the second parameter if the third doesn't exist
+	if (nullp(if_false)) {
+	  return eval(car(if_true));
+	} else {
+	  if (!nullp(cdr(if_false))) error_handler("s-expression invalid: if operator on more than three operands");
+	  
+	  Cell* condition_cell = eval(car(condition));
+	  bool flag = false;
+	  
+	  //retrieve values according to their types
+	  if (intp(condition_cell)){
+	    flag = get_int(condition_cell) ? true : false;
+	  } else if (doublep(condition_cell)) {
+	    flag = get_double(condition_cell) ? true : false;
+	  } else if (symbolp(condition_cell)) {
+	    flag = get_symbol(condition_cell)!="" ? true : false;
+	  } else {
+	    if(!nullp(car_value)) delete condition_cell;
+	    error_handler("s-expression invalid: condition operand invalid to if");
+	  }
+
+	  if(!nullp(car_value)) delete condition_cell;
+	  
+	  return flag ? eval(car(if_true)) : eval(car(if_false));
+	}
+      } else {
+	//delete car_value as it's no longer needed
+	delete car_value;
+	
+	error_handler("s-expression invalid: operator not one of +, ceiling or if");
+      }
+    } else {
+      //delete car_value as it's no longer needed
+      if(!nullp(car_value)) delete car_value;
+	
+      //value_car is not a symbol cell
+      error_handler("s-expression invalid: the first element of the tree/subtree is not a proper operator");
+    }
+  }
+}
