16e4e8b242bd71da6497b8dac4d9c00d1bd6488e
SUN Yushi <sunyushi9546@gmail.com>
2014-10-21 13:02:32 +0800

    change OperandNumberMismatchError interface

--- exceptions.cpp
+++ exceptions.cpp
@@ -10,10 +10,6 @@ DivideByZeroError::DivideByZeroError(string msg): runtime_error(msg){}
 
 OperandInvalidError::OperandInvalidError(string operation): runtime_error(operation + " operand invalid"){}
 
-OperandNumberMismatchError::OperandNumberMismatchError(string operation, int n): runtime_error(operation + " must have "+n+" operand(s)"){}
+OperandNumberMismatchError::OperandNumberMismatchError(string operation, string n): runtime_error(operation + " must have " + n +" operand(s)"){}
 
-OperandNumberMismatchError::OperandNumberMismatchError(string operation, int n, int m): runtime_error(operation + " must have "+n+" OR "+m+" operand(s)"){}
-
-OperandNumberMismatchError::OperandNumberMismatchError(string operation, int n, int m, int k): runtime_error(operation + " must have "+n+" OR "+m+" OR "+k+" operand(s)"){}
-
-UnimplementedError::UnimplementedError(string method_name): runtime_error(method_name + " method not implemented");
+UnimplementedError::UnimplementedError(string method_name): runtime_error(method_name + " method not implemented"){}
